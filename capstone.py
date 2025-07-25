4f5dad9e21a44b9e5a23fab5754b2f9d6003fff8
Deizel20214 <diseala@icloud.com>
2025-07-25 11:54:23 -0700

    Add files via upload

--- capstone.py
+++ capstone.py
@@ -5,6 +5,14 @@ def welcome_message():
     print("Welcome to the Grade calculator") 
     print("="*30)
 
+import random
+
+def welcome_message():
+    print("="*30)
+    print("Welcome to the Grade calculator")
+    print("="*30)
+
+welcome_message()
 
 # Function to run the multiplication guessing game
 def multiplication_function(max_attempts=3):
@@ -12,28 +20,6 @@ def multiplication_function(max_attempts=3):
     num1 = random.randint(1, 10)
     num2 = random.randint(1, 10)
     
-    
-    correct_answer = num1 * num2
-    
-    # Print "What times what gives you the answer?"
-    print(f"What times what gives you {correct_answer}?")
-    
-    # Make attempt = 0
-    attempts = 0
-
-    # Allow the user to guess for 3 number of attempts
-    while attempts < max_attempts:
-        # Ask the user for their guess number
-        guess_1 = int(input("jamal come up here and solve this division😏: "))
-
-
-# Function to run the multiplication guessing game
-def multiplication_function(max_attempts=3):
-    # Generate a random number for the left side of the problem
-    num1 = random.randint(1, 10)
-    
-    # Generate the correct answer by multiplying num1 by another random number
-    num2 = random.randint(1, 10)
     correct_answer = num1 * num2
     
     # Print the structure of the equation
@@ -45,21 +31,19 @@ def multiplication_function(max_attempts=3):
     # Allow the user to guess for a limited number of attempts
     while attempts < max_attempts:
         # Ask the user to guess the second number (num2)
-        guess = int(input(" somone else go answer  the second equation: "))
+        guess = int(input("Jamal, come up here and solve this division 😏: "))
         attempts += 1
         
         # Check if the guess is correct
         if guess == num2:
-            print(f"great job jamal!!😁 {num1} * {num2} is {correct_answer}.")
+            print(f"Great job Jamal!! 😁 {num1} * {num2} is {correct_answer}.")
             break
         else:
-            print(f"wrong answer you have failed get out of my class room right now!!!😡 {max_attempts - attempts} attempts left.")
+            print(f"Wrong answer! You have failed. Get out of my classroom right now!!! 😡 {max_attempts - attempts} attempts left.")
     
-    # If statement for whe user runs out of attempts
+    # If the user runs out of attempts
     if attempts == max_attempts and guess != num2:
-        print(f"you are out of attepts stop skipping school!!!!!💀{num2}.")
+        print(f"You are out of attempts! Stop skipping school!!!!! 💀 The correct answer was {num2}.")
 
 # Call and run the game
 multiplication_function(max_attempts=3)
-
-  
